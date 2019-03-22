9708ed7ccd5aec559afcff43366647cb035ce39e
CodingCherry <cherry.alexeeva@gmail.com>
2019-03-22 18:42:17 +0700

    Login VC UI

---
+++ Smack-chat-app/Controller/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  Smack-chat-app
+//
+//  Created by Alexeeva Ekaterina on 3/22/19.
+//  Copyright © 2019 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+
+
+}
---
+++ Smack-chat-app/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack-chat-app
+//
+//  Created by Alexeeva Ekaterina on 3/22/19.
+//  Copyright © 2019 Alexeeva Ekaterina. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
